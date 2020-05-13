3de94dd914071204318cf9e1979e5e1edb5a650b
[evibeelen] <e.beelen@tilburguniversity.edu>
2020-05-13 10:24:12 +0200

    modified parse file v5

--- src/data-preparation/parse.py
+++ src/data-preparation/parse.py
@@ -1,24 +1,44 @@
 import json
 
-f = open('../../gen/data-preparation/temp/fortnite_astronomical_dataset/fortnite_allevent.json','r', encoding='utf-8')
+f = open('../../gen/data-preparation/temp/public-attitudes-press-conference.json','r', encoding='utf-8')
 
 con = f.readlines()
 
 outfile = open('../../gen/data-preparation/temp/parsed-data.csv', 'w', encoding = 'utf-8')
 
-outfile.write('id\tcreated_at\ttext\n')
+outfile.write('id\tcreated_at\ttext\tscholen\tcontactberoep\tverpleeghuis\thoreca\tevenementen\n')
 
-cnt = 0
-for line in con:
-    if (len(line)<=5): continue
+for i in con:
+    school=0
+    contact=0
+    verpleeg=0
+    horeca=0
+    evenement=0
+    if ('{' not in i):
+        continue
+    obj=json.loads(i)
+    try:
+        text = obj.get("retweeted_status").get("extended_tweet").get("full_text")
+    except:
+        try:
+            text = obj.get("extended_tweet").get("full_text")
+        except:
+            try:
+                text = obj.get("quoted_status").get("extended_tweet").get("full_text")
+            except:
+                text = obj.get("text" )
+    text = text.replace('\t', ' ').replace('\n', ' ').replace(';', ' ')
+    if "school" in text.lower() or "onderwijs" in text.lower() or "scholen" in text.lower():
+        school=1
+    if "kapper" in text.lower() or "contactberoep" in text.lower():
+        contact=1
+    if "verpleeghuis" in text.lower() or "verpleeghuizen" in text.lower():
+        verpleeg=1
+    if "horeca" in text.lower() or "restaurant" in text.lower() or "terras" in text.lower() or "kroeg" in text.lower() or "bar" in text.lower():
+        horeca=1
+    if "evenement" in text.lower() or "festival" in text.lower():
+        evenement=1
 
-    cnt+=1
-    obj = json.loads(line.replace('\n',''))
-
-    text = obj.get('text')
-    text = text.replace('\t', '').replace('\n', '')
-
-    outfile.write(obj.get('id_str')+'\t'+obj.get('created_at')+'\t'+text+'\n')
-    if (cnt>1000): break
+    outfile.write(obj.get('id_str')+'\t'+obj.get('created_at')+'\t'+text+'\t'+str(school)+'\t'+str(contact)+'\t'+str(verpleeg)+'\t'+str(horeca)+'\t'+str(evenement)+'\n')
 
 print('done.')
